f14a38c36d16c7b84ad9f0b2ff484796bf0d7f9f
Christian Hiland <hilandchristian112@gmail.com>
2023-06-04 15:04:31 -0400

    Update Main.py

--- Code/Main.py
+++ Code/Main.py
@@ -14,10 +14,10 @@ client = commands.Bot(command_prefix="$") # '$' is the prefix tha the user will 
 # Printing this out on the terminal if the bot is online.
 @client.event
 async def on_ready():
-			print("Lunbin Bot is online!")
+	print("Lunbin Bot is online!")
 
 @client.command()
 async def ping(ctx):
-			await ctx.send("pong!") # When a user types '$ping' the bot will send 'pong!'
+	await ctx.send("pong!") # When a user types '$ping' the bot will send 'pong!'
 
 client.run("BOT_TOKEN")

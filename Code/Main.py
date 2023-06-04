44896934173cedf2a11a24a01b10e1ef7169adba
Christian Hiland <hilandchristian112@gmail.com>
2023-06-04 15:11:32 -0400

    Update Main.py

--- Code/Main.py
+++ Code/Main.py
@@ -2,14 +2,22 @@
 import discord
 import os
 import time
-import discord.ext
 import platform
+import json
+import discord.ext
 from discord.utils import get
 from discord.ext import commands, tasks
 from discord.ext.commands import has_permissions, CheckFailure, check
 
+# Reading the Config file.
+configJSON = "Config/Config.json"
+config = open(configJSON, "r")
+BOT_PREFIX = config['Prefix']
+BOT_TOKEN = config['TOKEN']
+
 client = discord.Client()
-client = commands.Bot(command_prefix="$") # '$' is the prefix tha the user will type to active the bot.
+client = commands.Bot(command_prefix=BOT_PREFIX) # BOT_PREFIX is the prefix tha the user will type to active the bot.
+
 
 # Printing this out on the terminal if the bot is online.
 @client.event
@@ -23,4 +31,4 @@ async def on_ready():
 async def ping(ctx):
 	await ctx.send("pong!") # When a user types '$ping' the bot will send 'pong!'
 
-client.run("BOT_TOKEN")
+client.run(BOT_TOKEN)
